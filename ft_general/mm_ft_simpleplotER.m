ce3cb08e294d4ca241ae0703c72f53aeb68173ef
Matt Mollison <matt.mollison@gmail.com>
2014-01-04 21:10:18 -0700

    trying new format

--- ft_general/mm_ft_simpleplotER.m
+++ ft_general/mm_ft_simpleplotER.m
@@ -1,4 +1,4 @@
-function mm_ft_simpleplotER(cfg_ft,cfg_plot,ana,exper,data)
+function mm_ft_simpleplotER(cfg_ft,cfg_plot,ana,exper,sesNum,data)
 %MM_FT_SIMPLEPLOTER Simple plot of ERP data
 %   
 
@@ -33,36 +33,53 @@ elseif isfield(cfg_plot,'roi')
   end
 end
 
-% make sure it's set up for the for loop
-if ~isfield(cfg_plot,'condMethod')
-  if ~iscell(cfg_plot.conditions) && (strcmp(cfg_plot.conditions,'all') || strcmp(cfg_plot.conditions,'all_across_types') || strcmp(cfg_plot.conditions,'all_within_types'))
-    cfg_plot.condMethod = 'single';
-  elseif iscell(cfg_plot.conditions) && ~iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-    cfg_plot.condMethod = 'single';
-  elseif iscell(cfg_plot.conditions) && iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions{1}) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-    cfg_plot.condMethod = 'single';
-  else
-    cfg_plot.condMethod = [];
-  end
-end
-cfg_plot.conditions = mm_ft_checkConditions(cfg_plot.conditions,ana,cfg_plot.condMethod);
+% % make sure it's set up for the for loop
+% if ~isfield(cfg_plot,'condMethod')
+%   if ~iscell(cfg_plot.conditions) && (strcmp(cfg_plot.conditions,'all') || strcmp(cfg_plot.conditions,'all_across_types') || strcmp(cfg_plot.conditions,'all_within_types'))
+%     cfg_plot.condMethod = 'single';
+%   elseif iscell(cfg_plot.conditions) && ~iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
+%     cfg_plot.condMethod = 'single';
+%   elseif iscell(cfg_plot.conditions) && iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions{1}) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
+%     cfg_plot.condMethod = 'single';
+%   else
+%     cfg_plot.condMethod = [];
+%   end
+% end
+% cfg_plot.conditions = mm_ft_checkConditions(cfg_plot.conditions,ana,cfg_plot.condMethod);
 
-for typ = 1:length(cfg_plot.conditions)
+% for typ = 1:length(cfg_plot.conditions)
+%   cfg = [];
+%   cfg.conditions = cfg_plot.conditions{typ};
+%   cfg.data_str = 'data';
+%   cfg.is_ga = cfg_plot.is_ga;
+%   cfg.excludeBadSub = cfg_plot.excludeBadSub;
+%   ana_str = mm_ft_catSubStr(cfg,exper);
+%   
+%   figure
+%   eval(sprintf('ft_singleplotER(cfg_ft,%s);',ana_str));
+%   legend(strrep(cfg_plot.conditions{typ},'_','-'),'Location',cfg_plot.legendloc);
+%   set(gcf,'Name',sprintf(repmat('%s ',1,length(cfg_plot.roi)),cfg_plot.roi{:}));
+%   
+%   plot([cfg_ft.xlim(1) cfg_ft.xlim(2)],[0 0],'k--'); % horizontal
+%   plot([0 0],[cfg_ft.ylim(1) cfg_ft.ylim(2)],'k--'); % vertical
+% end
+
+% for typ = 1:length(cfg_plot.conditions)
   cfg = [];
-  cfg.conditions = cfg_plot.conditions{typ};
+  cfg.conditions = cfg_plot.conditions;
   cfg.data_str = 'data';
   cfg.is_ga = cfg_plot.is_ga;
   cfg.excludeBadSub = cfg_plot.excludeBadSub;
-  ana_str = mm_ft_catSubStr(cfg,exper);
+  ana_str = mm_catSubStr_multiSes(cfg,exper,sesNum);
   
   figure
   eval(sprintf('ft_singleplotER(cfg_ft,%s);',ana_str));
-  legend(strrep(cfg_plot.conditions{typ},'_','-'),'Location',cfg_plot.legendloc);
+  legend(strrep(cfg_plot.conditions,'_','-'),'Location',cfg_plot.legendloc);
   set(gcf,'Name',sprintf(repmat('%s ',1,length(cfg_plot.roi)),cfg_plot.roi{:}));
   
   plot([cfg_ft.xlim(1) cfg_ft.xlim(2)],[0 0],'k--'); % horizontal
   plot([0 0],[cfg_ft.ylim(1) cfg_ft.ylim(2)],'k--'); % vertical
-end
+% end
 
 end
 
