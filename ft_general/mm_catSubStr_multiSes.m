97b98fe6b35e57d290338b210bc8b3d26454eb38
Matt Mollison <matt.mollison@gmail.com>
2014-01-04 21:03:00 -0700

    include session number

--- ft_general/mm_catSubStr_multiSes.m
+++ ft_general/mm_catSubStr_multiSes.m
@@ -1,8 +1,8 @@
-function [ana_str] = mm_catSubStr_multiSes(cfg,exper)
+function [ana_str] = mm_catSubStr_multiSes(cfg,exper,ses)
 %MM_CATSUBSTR_MULTISES Concatenate strings of subject data for input to FieldTrip
 %functions
 %
-% [ana_str] = mm_catSubStr_multiSes(cfg,exper)
+% [ana_str] = mm_catSubStr_multiSes(cfg,exper,ses)
 %
 % input:
 %   cfg.conditions    = which condition fields to create in ana_str; should
@@ -13,6 +13,7 @@ function [ana_str] = mm_catSubStr_multiSes(cfg,exper)
 %                       (1 or 0)
 %   cfg.excludeBadSub = 1 or 0; excludes bad subjects by default
 %   exper             = exper struct with subject, session, and badSub info
+%   ses               = session number
 %
 % output:
 %   ana_str is a struct with fields corresponding to event values in
@@ -27,12 +28,8 @@ function [ana_str] = mm_catSubStr_multiSes(cfg,exper)
 if ~iscell(cfg.conditions) && ischar(cfg.conditions)
   cfg.conditions = {cfg.conditions};
 end
-if iscell(cfg.conditions) && ~iscell(cfg.conditions{1})
-  cfg.conditions = {cfg.conditions};
-end
-% % concatenate the types together if they were separate
-% if iscell(cfg.conditions) && iscell(cfg.conditions{1})
-%   cfg.conditions = cat(2,cfg.conditions{:});
+% if iscell(cfg.conditions) && ~iscell(cfg.conditions{1})
+%   cfg.conditions = {cfg.conditions};
 % end
 
 if nargin < 2
@@ -55,7 +52,7 @@ if ~cfg.is_ga
   firstOneDone = 0;
   
   % go through subjects, add strings for the ones we want
-  for ses = 1:length(exper.sessions)
+  %for ses = 1:length(exper.sessions)
     for sub = 1:length(exper.subjects)
       if exper.badSub(sub,ses)
         if cfg.excludeBadSub
@@ -69,28 +66,40 @@ if ~cfg.is_ga
       else
         if ~firstOneDone
           % add the first subject; the string is formatetted differently
-          for evVal = 1:length(cfg.conditions{ses})
-            ana_str.(cfg.conditions{ses}{evVal}){ses} = sprintf('%s.%s.%s.sub(%d).data',cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{evVal},sub);
+          %for evVal = 1:length(cfg.conditions{ses})
+          %  ana_str.(cfg.conditions{ses}{evVal}){ses} = sprintf('%s.%s.%s.sub(%d).data',cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{evVal},sub);
+          %end
+          for evVal = 1:length(cfg.conditions)
+            ana_str.(cfg.conditions{evVal}) = sprintf('%s.%s.%s.sub(%d).data',cfg.data_str,exper.sesStr{ses},cfg.conditions{evVal},sub);
           end
           firstOneDone = 1;
         else
-          for evVal = 1:length(cfg.conditions{ses})
-            ana_str.(cfg.conditions{ses}{evVal}){ses} = sprintf('%s,%s.%s.%s.sub(%d).data',ana_str.(cfg.conditions{ses}{evVal}){ses},cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{evVal},sub);
+          %for evVal = 1:length(cfg.conditions{ses})
+          %  ana_str.(cfg.conditions{ses}{evVal}){ses} = sprintf('%s,%s.%s.%s.sub(%d).data',ana_str.(cfg.conditions{ses}{evVal}){ses},cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{evVal},sub);
+          %end
+          for evVal = 1:length(cfg.conditions)
+            ana_str.(cfg.conditions{evVal}) = sprintf('%s,%s.%s.%s.sub(%d).data',ana_str.(cfg.conditions{evVal}),cfg.data_str,exper.sesStr{ses},cfg.conditions{evVal},sub);
           end
         end
       end
     end % sub
-  end % ses
+  %end % ses
 else
   % if we have grand average data, then we just need the event value names
-  for ses = 1:length(exper.sessions)
-    ana_str = sprintf('%s.%s.%s',cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{1});
-    if length(cfg.conditions{ses}) > 1
-      for evVal = 2:length(cfg.conditions{ses})
-        ana_str = sprintf('%s,%s.%s.%s',ana_str,cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{evVal});
+  %for ses = 1:length(exper.sessions)
+    %ana_str = sprintf('%s.%s.%s',cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{1});
+    %if length(cfg.conditions{ses}) > 1
+    %  for evVal = 2:length(cfg.conditions{ses})
+    %    ana_str = sprintf('%s,%s.%s.%s',ana_str,cfg.data_str,exper.sesStr{ses},cfg.conditions{ses}{evVal});
+    %  end
+    %end
+    ana_str = sprintf('%s.%s.%s',cfg.data_str,exper.sesStr{ses},cfg.conditions{1});
+    if length(cfg.conditions) > 1
+      for evVal = 2:length(cfg.conditions)
+        ana_str = sprintf('%s,%s.%s.%s',ana_str,cfg.data_str,exper.sesStr{ses},cfg.conditions{evVal});
       end
     end
-  end
+  %end
 end
 
 end
