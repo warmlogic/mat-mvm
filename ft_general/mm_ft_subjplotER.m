90a1e24b44d4ab018f94e7b0b1d4178f66e71c21
Matt Mollison <matt.mollison@gmail.com>
2014-02-13 00:30:35 -0700

    support for multiple sessions

--- ft_general/mm_ft_subjplotER.m
+++ ft_general/mm_ft_subjplotER.m
@@ -1,9 +1,9 @@
-function mm_ft_subjplotER(cfg_plot,ana,exper,data)
+function mm_ft_subjplotER(cfg_plot,ana,exper,data,ses)
 %MM_FT_SUBPLOTER make a subplot of the individual subject data
 %   
 
 % FIXME: implement support for multiple sessions
-ses = 1;
+% ses = 1;
 
 vertTextLoc = 0.18;
 
@@ -18,31 +18,34 @@ end
 if ~isfield(cfg_plot,'numCols')
   cfg_plot.numCols = 5;
 end
+if ~isfield(cfg_plot,'titleTrialCount')
+  cfg_plot.titleTrialCount = true;
+end
 if ~isfield(cfg_plot,'graphcolor')
   cfg_plot.graphcolor = 'rbkgcmyrbkgcmyrbkgcmy';
 end
 
-% make sure conditions are set correctly
-if ~isfield(cfg_plot,'condMethod')
-  if ~iscell(cfg_plot.conditions) && (strcmp(cfg_plot.conditions,'all') || strcmp(cfg_plot.conditions,'all_across_types') || strcmp(cfg_plot.conditions,'all_within_types'))
-    cfg_plot.condMethod = 'single';
-  elseif iscell(cfg_plot.conditions) && ~iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-    cfg_plot.condMethod = 'single';
-  elseif iscell(cfg_plot.conditions) && iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions{1}) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-    cfg_plot.condMethod = 'single';
-  else
-    cfg_plot.condMethod = [];
-  end
-end
-cfg_plot.conditions = mm_ft_checkConditions(cfg_plot.conditions,ana,cfg_plot.condMethod);
-% make sure conditions are set up for the for loop
-if ~isfield(cfg_plot,'types')
-  cfg_plot.types = repmat({''},size(cfg_plot.conditions));
-end
+% % make sure conditions are set correctly
+% if ~isfield(cfg_plot,'condMethod')
+%   if ~iscell(cfg_plot.conditions) && (strcmp(cfg_plot.conditions,'all') || strcmp(cfg_plot.conditions,'all_across_types') || strcmp(cfg_plot.conditions,'all_within_types'))
+%     cfg_plot.condMethod = 'single';
+%   elseif iscell(cfg_plot.conditions) && ~iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
+%     cfg_plot.condMethod = 'single';
+%   elseif iscell(cfg_plot.conditions) && iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions{1}) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
+%     cfg_plot.condMethod = 'single';
+%   else
+%     cfg_plot.condMethod = [];
+%   end
+% end
+% cfg_plot.conditions = mm_ft_checkConditions(cfg_plot.conditions,ana,cfg_plot.condMethod);
+% % make sure conditions are set up for the for loop
+% if ~isfield(cfg_plot,'types')
+%   cfg_plot.types = repmat({''},size(cfg_plot.conditions));
+% end
 
 % get the label info for this data struct
-if isfield(data.(cfg_plot.conditions{1}{1}).sub(1).ses(1).data,'label')
-  lab = data.(cfg_plot.conditions{1}{1}).sub(1).ses(1).data.label;
+if isfield(data.(exper.sesStr{ses}).(cfg_plot.conditions{1}{1}{1}).sub(1).data,'label')
+  lab = data.(exper.sesStr{ses}).(cfg_plot.conditions{1}{1}{1}).sub(1).data.label;
 else
   error('label information not found in data struct\n');
 end
@@ -86,7 +89,7 @@ if (cfg_plot.numCols * cfg_plot.numRows) == length(exper.subjects)
 end
 
 % each entry in cfg_plot.conditions is a cell containing one type of events
-for typ = 1:length(cfg_plot.conditions)
+for typ = 1:length(cfg_plot.conditions{ses})
   figure
   for sub = 1:length(exper.subjects)
     if cfg_plot.excludeBadSub == 1 && exper.badSub(sub,ses)
@@ -100,19 +103,25 @@ for typ = 1:length(cfg_plot.conditions)
       
       evStr = [];
       % go through each event value for this type
-      for evVal = 1:length(cfg_plot.conditions{typ})
-        evStr = cat(2,evStr,sprintf(' %s:%d',strrep(cfg_plot.conditions{typ}{evVal},'_',''),exper.nTrials.(cfg_plot.conditions{typ}{evVal})(sub)));
-        if isfield(data.(cfg_plot.conditions{typ}{evVal}).sub(sub).ses(ses).data,cfg_plot.parameter)
-          plot(data.(cfg_plot.conditions{typ}{evVal}).sub(sub).ses(ses).data.time,mean(data.(cfg_plot.conditions{typ}{evVal}).sub(sub).ses(ses).data.(cfg_plot.parameter)(cfg_plot.chansel,:),1),cfg_plot.graphcolor(evVal));
+      for evVal = 1:length(cfg_plot.conditions{ses}{typ})
+        if cfg_plot.titleTrialCount
+          evStr = cat(2,evStr,sprintf(' %s:%d',strrep(cfg_plot.conditions{ses}{typ}{evVal},'_',''),exper.nTrials.(exper.sesStr{ses}).(cfg_plot.conditions{ses}{typ}{evVal})(sub)));
+        end
+        if isfield(data.(exper.sesStr{ses}).(cfg_plot.conditions{ses}{typ}{evVal}).sub(sub).data,cfg_plot.parameter)
+          plot(data.(exper.sesStr{ses}).(cfg_plot.conditions{ses}{typ}{evVal}).sub(sub).data.time,mean(data.(exper.sesStr{ses}).(cfg_plot.conditions{ses}{typ}{evVal}).sub(sub).data.(cfg_plot.parameter)(cfg_plot.chansel,:),1),cfg_plot.graphcolor(evVal));
         end
       end
       
+      titleStr = sprintf('%s',exper.subjects{sub});
+      
       if cfg_plot.excludeBadSub == 0 && exper.badSub(sub,ses)
         fprintf('Found bad subject: %s\n',exper.subjects{sub});
-        title(sprintf('*BAD*: %s;%s',exper.subjects{sub},evStr))
-      else
-        title(sprintf('%s;%s',exper.subjects{sub},evStr))
+        titleStr = sprintf('*BAD*: %s',titleStr);
       end
+      if cfg_plot.titleTrialCount
+        titleStr = sprintf('%s;%s',titleStr,evStr);
+      end
+      title(titleStr);
       
       axis([cfg_plot.xlim(1) cfg_plot.xlim(2) cfg_plot.ylim(1) cfg_plot.ylim(2)]);
       hold off
@@ -128,8 +137,8 @@ for typ = 1:length(cfg_plot.conditions)
   end
   text(0.5,ycoord,sprintf(repmat('%s ',1,length(cfg_plot.roi)),cfg_plot.roi{:}),'color','k');
   ycoord = ycoord - vertTextLoc;
-  for evVal = 1:length(cfg_plot.conditions{typ})
-    text(0.5,ycoord,strrep(cfg_plot.conditions{typ}{evVal},'_',''),'color',cfg_plot.graphcolor(evVal));
+  for evVal = 1:length(cfg_plot.conditions{ses}{typ})
+    text(0.5,ycoord,strrep(cfg_plot.conditions{ses}{typ}{evVal},'_',''),'color',cfg_plot.graphcolor(evVal));
     ycoord = ycoord - vertTextLoc;
   end
   axis off
