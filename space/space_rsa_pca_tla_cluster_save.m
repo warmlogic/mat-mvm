3cc5a72a4b1ab5309720500586de8d10bfed024e
Matt Mollison <matt.mollison@gmail.com>
2014-05-04 10:28:04 -0600

    no exper

--- space/space_rsa_pca_tla_cluster_save.m
+++ space/space_rsa_pca_tla_cluster_save.m
@@ -72,7 +72,7 @@ thisDate = '03-May-2014';
 
 for sub = 1:length(subjects)
   for ses = 1:length(sesNames)
-    savedFile = fullfile(saveDirProc,exper.subjects{sub},exper.sesStr{ses},sprintf('RSA_PCA_tla_classif_%s_%s_%dlat_%sAvgT_%s.mat',eig_criterion,roi_str,size(latencies,1),cfg_sel.avgovertime,thisDate));
+    savedFile = fullfile(saveDirProc,subjects{sub},sesNames{ses},sprintf('RSA_PCA_tla_classif_%s_%s_%dlat_%sAvgT_%s.mat',eig_criterion,roi_str,size(latencies,1),cfg_sel.avgovertime,thisDate));
     subData = load(savedFile);
     
     exper = subData.exper;
