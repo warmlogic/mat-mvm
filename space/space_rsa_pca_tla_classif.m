82097868abbfd3f6cb556adad5c827f73d168f2f
Matt Mollison <matt.mollison@gmail.com>
2014-05-24 20:33:16 -0600

    all times

--- space/space_rsa_pca_tla_classif.m
+++ space/space_rsa_pca_tla_classif.m
@@ -203,7 +203,7 @@ latencies = [0.0 0.2; 0.2 0.4; 0.4 0.6; 0.6 0.8; 0.8 1.0; ...
   0 0.8; 0.1 0.9; 0.2 1.0;
   0 1.0];
 
-latencies = [0.0 0.5; 0.5 1.0];
+% latencies = [0.0 0.5; 0.5 1.0];
 
 % column numbers in trialinfo
 % trialNumCol = 5;
