893307ac1da2c0279cdb829db48bb1957191f7e1
Matt Mollison <matt.mollison@gmail.com>
2014-06-22 13:07:38 -0600

    updates for peak finding

--- ft_general/mm_findPeak.m
+++ ft_general/mm_findPeak.m
@@ -1,4 +1,4 @@
-function mm_findPeak(cfg,ana,exper,data,cfg_ft)
+function mm_findPeak(cfg,ana,exper,data,cfg_plot)
 
 % time
 %
@@ -7,7 +7,7 @@ function mm_findPeak(cfg,ana,exper,data,cfg_ft)
 % the only real field option for cfg_ft is cfg_ft.channel
 
 if nargin < 5
-  cfg_ft = [];
+  cfg_plot = [];
 end
 
 if all(~ismember(cfg.datadim,{'elec','time','peak2peak'}))
@@ -26,7 +26,6 @@ end
 
 if ~isfield(cfg,'conditions')
   cfg.conditions = cellflat(ana.eventValues{1});
-  fprintf('Finding peak across all conditions:%s\n',sprintf(repmat(' %s',1,length(cfg_plot.conditions)),cfg_plot.conditions{:}));
 end
 
 if ~isfield(cfg,'is_ga')
@@ -44,15 +43,24 @@ if ~isfield(cfg,'sesNum')
   fprintf('cfg.sesNum unset. defaulting to %d\n',cfg.sesNum);
 end
 
-if ~isfield(cfg_ft,'channel')
-  if ~isfield(cfg,'roi')
-    cfg.roi = 'center109';
-    fprintf('cfg.roi unset. defaulting to ''%s''\n',cfg.roi);
-  end
-  cfg_ft.channel = cat(2,ana.elecGroups{ismember(ana.elecGroupsStr,cfg.roi)});
+if ~isfield(cfg,'voltlim')
+  cfg.voltlim = [-5 5];
 end
 
-% if ~isfield(cfg,'latency') || (isfield(cfg,'latency') && isempty(cfg.latency))
+if ~isfield(cfg,'plotit')
+  cfg.plotit = true;
+end
+if strcmp(cfg.datadim,'elec') && cfg.plotit && ~isfield(cfg,'plottype')
+  cfg.plottype = 'topo';
+end
+
+% set the electrodes and times
+cfg_ft = [];
+if ~isfield(cfg,'roi')
+  cfg.roi = 'center101';
+  fprintf('cfg.roi unset. defaulting to ''%s''\n',cfg.roi);
+end
+cfg_ft.channel = cat(2,ana.elecGroups{ismember(ana.elecGroupsStr,cfg.roi)});
 if ~strcmp(cfg.datadim,'peak2peak')
   if ~isfield(cfg,'latency')
     cfg.latency = 'all';
@@ -63,10 +71,9 @@ if ~strcmp(cfg.datadim,'peak2peak')
 elseif strcmp(cfg.datadim,'peak2peak')
   cfg_ft.latency = 'all';
 end
-% end
 
+% set up the data string
 cfg.data_str = 'data';
-
 ana_str = mm_catSubStr_multiSes2(cfg,exper,cfg.sesNum);
 
 % % %cfg_ft.channel = cat(2,ana.elecGroups{ismember(ana.elecGroupsStr,{'posterior'})});
@@ -76,24 +83,24 @@ ana_str = mm_catSubStr_multiSes2(cfg,exper,cfg.sesNum);
 % cfg_ft.channel = cat(2,ana.elecGroups{ismember(ana.elecGroupsStr,{'RPI2'})});
 % cfg_ft.channel = cat(2,ana.elecGroups{ismember(ana.elecGroupsStr,{'RPI3'})});
 
-
 % ga_allCond = eval(sprintf('ft_timelockgrandaverage(cfg_ft,%s);',ana_str));
 
+fprintf('Finding peak across conditions:%s\n',sprintf(repmat(' %s',1,length(cfg.conditions)),cfg.conditions{:}));
+
+fprintf('Concatenating...');
 allCond = eval(sprintf('ft_appenddata([],%s);',ana_str));
+fprintf('Done.\n');
+
+fprintf('Converting...');
 ga_allCond = ft_timelockanalysis([],allCond);
-
-if ~isfield(cfg,'plotit')
-  cfg.plotit = true;
-end
-
-if strcmp(cfg.datadim,'elec') && cfg.plotit && ~isfield(cfg,'plottype')
-  cfg.plottype = 'multi';
-end
+fprintf('Done.\n');
 
 if strcmp(cfg.datadim,'elec')
   % do this first to find the peak electrode/ROI
   
+  fprintf('Selecting data of interest...');
   ga_data = ft_selectdata_new(cfg_ft,ga_allCond);
+  fprintf('Done.\n');
   
   if ~isfield(cfg,'order')
     cfg.order = 'descend';
@@ -106,93 +113,106 @@ if strcmp(cfg.datadim,'elec')
   disp(y');
   
   if cfg.plotit
-    if ~isfield(cfg_ft,'layout') && isfield(ana,'elec')
-      cfg_ft.layout = ft_prepare_layout([],ana);
+    fprintf('Plotting...');
+    
+    if ~isfield(cfg_plot,'layout') && isfield(ana,'elec')
+      cfg_plot.layout = ft_prepare_layout([],ana);
     end
-    if ~isfield(cfg_ft,'xlim')
+    if ~isfield(cfg_plot,'xlim')
       if strcmp(cfg.latency,'all')
-        cfg_ft.xlim = 'maxmin';
+        cfg_plot.xlim = 'maxmin';
       else
-        cfg_ft.xlim = cfg.latency;
+        cfg_plot.xlim = cfg_ft.latency;
       end
+    end
+    if ~isfield(cfg_plot,'interactive')
+      % cfg_ft.interactive = 'yes';
+      cfg_plot.interactive = 'no';
     end
     
     if strcmp(cfg.plottype,'topo')
       % topoplot
       
-      cfg_ft.highlightchannel = cfg_ft.channel;
-      cfg_ft.channel = 'all';
-      cfg_ft.highlight = 'labels';
+      cfg_plot.highlightchannel = cfg_ft.channel;
+      cfg_plot.channel = 'all';
+      cfg_plot.highlight = 'labels';
+      cfg_plot.colorbar = 'yes';
       
       %cfg_ft.highlight = 'on';
       %cfg_ft.highlightsize = 10;
       %cfg_ft.highlightsymbol = '*';
       
-      if ~isfield(cfg_ft,'zlim') && isfield(cfg,'zlim')
-        cfg_ft.zlim = cfg.zlim;
+      if ~isfield(cfg_plot,'zlim')
+        cfg_plot.zlim = cfg.voltlim;
       end
-      if ~isfield(cfg_ft,'marker')
+      if ~isfield(cfg_plot,'marker')
         if isfield(cfg,'marker')
-          cfg_ft.marker = cfg.marker;
+          cfg_plot.marker = cfg.marker;
         else
-          cfg_ft.marker = 'on';
+          cfg_plot.marker = 'on';
           %cfg_ft.marker = 'label';
         end
       end
-      if ~isfield(cfg_ft,'markerfontsize')
-        cfg_ft.markerfontsize = 9;
+      if ~isfield(cfg_plot,'markerfontsize')
+        cfg_plot.markerfontsize = 10;
       end
       
       figure
-      ft_topoplotER(cfg_ft,ga_allCond);
+      ft_topoplotER(cfg_plot,ga_data);
       
     elseif strcmp(cfg.plottype,'multi')
       % multiplot
-      if ~isfield(cfg_ft,'ylim')
-        if isfield(cfg,'ylim')
-          cfg_ft.ylim = cfg.ylim;
-        else
-          cfg_ft.ylim = [-5 5];
-        end
+      if ~isfield(cfg_plot,'ylim')
+        cfg_plot.ylim = cfg.voltlim;
       end
-      if ~isfield(cfg_ft,'showlabels')
-        cfg_ft.showlabels = 'yes';
+      if ~isfield(cfg_plot,'showlabels')
+        cfg_plot.showlabels = 'yes';
       end
-      if ~isfield(cfg_ft,'interactive')
-        % cfg_ft.interactive = 'yes';
-        cfg_ft.interactive = 'no';
+      if ~isfield(cfg_plot,'showoutline')
+        cfg_plot.showoutline = 'yes';
       end
-      if ~isfield(cfg_ft,'showoutline')
-        cfg_ft.showoutline = 'yes';
-      end
-      if ~isfield(cfg_ft,'fontsize')
-        cfg_ft.fontsize = 9;
+      if ~isfield(cfg_plot,'fontsize')
+        cfg_plot.fontsize = 9;
       end
       
       figure
-      ft_multiplotER(cfg_ft,ga_allCond);
+      ft_multiplotER(cfg_plot,ga_allCond);
     end
+    fprintf('Done.\n');
   end
   
 elseif strcmp(cfg.datadim,'time')
   % do this once you have nailed down which is the peak electrode
   
   if cfg.plotit
+    fprintf('Plotting...');
+    if ~isfield(cfg_plot,'xlim')
+      if strcmp(cfg.latency,'all')
+        cfg_plot.xlim = 'maxmin';
+      else
+        cfg_plot.xlim = cfg.latency;
+      end
+    end
+    
+    if ~isfield(cfg_plot,'ylim')
+      cfg_plot.ylim = cfg.voltlim;
+    end
+    
+    if ~isfield(cfg_plot,'interactive')
+      % cfg_ft.interactive = 'yes';
+      cfg_plot.interactive = 'no';
+    end
+      
     figure
-    ft_singleplotER(cfg_ft,ga_allCond);
+    ft_singleplotER(cfg_plot,ga_allCond);
+    fprintf('Done.\n');
   end
   
 elseif strcmp(cfg.datadim,'peak2peak')
   
-  %   pospeak = [0.1 0.15];
-  %
-  %   negpeak = [0.15 0.2];
-  
-  
   cfg_sd = [];
   cfg_sd.latency = cfg.pospeak;
   ga_pos = ft_selectdata_new(cfg_sd,ga_allCond);
-  
   
   cfg_sd = [];
   cfg_sd.latency = cfg.negpeak;
@@ -215,5 +235,4 @@ elseif strcmp(cfg.datadim,'peak2peak')
   disp(y');
   
 end
-
 
