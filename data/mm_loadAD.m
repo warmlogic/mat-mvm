6a1969775e04edccc08b5831651024ed75a51bfd
Matt Mollison <matt.mollison@gmail.com>
2014-07-09 11:35:31 -0600

    support for full trialinfo including artifact trials

--- data/mm_loadAD.m
+++ data/mm_loadAD.m
@@ -133,6 +133,9 @@ for ses = 1:length(sesStr)
             if isfield(exper,'artifacts')
               exper.artifacts = rmfield(exper.artifacts,exper.sesStr{k});
             end
+            if isfield(exper,'trialinfo_allEv')
+              exper.trialinfo_allEv = rmfield(exper.trialinfo_allEv,exper.sesStr{k});
+            end
           end
         end
       end
@@ -144,6 +147,9 @@ for ses = 1:length(sesStr)
 %           exper.badEv.(sesNames{ses}) = rmfield(exper.badEv.(sesNames{ses}),exper.eventValues{ses}{evVal});
 %           if isfield(exper,'artifacts')
 %             exper.artifacts.(sesNames{ses}) = rmfield(exper.artifacts.(sesNames{ses}),exper.eventValues{ses}{evVal});
+%           end
+%           if isfield(exper,'trialinfo_allEv')
+%             exper.trialinfo_allEv.(sesNames{ses}) = rmfield(exper.trialinfo_allEv.(sesNames{ses}),exper.eventValues{ses}{evVal});
 %           end
 %         end
 %       end
@@ -167,6 +173,9 @@ for ses = 1:length(sesStr)
         exper.badEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}) = sd.exper.badEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal})(sub);
         if isfield(sd.exper,'artifacts')
           exper.artifacts.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}) = sd.exper.artifacts.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal})(sub);
+        end
+        if isfield(sd.exper,'trialinfo_allEv')
+          exper.trialinfo_allEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}) = sd.exper.trialinfo_allEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal})(sub);
         end
       end
       
@@ -192,6 +201,9 @@ if length(subjects) > 1
           exper.badEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}) = cat(1,exper.badEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}),sd.exper.badEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}));
           if isfield(sd.exper,'artifacts')
             exper.artifacts.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}) = cat(1,exper.artifacts.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}),sd.exper.artifacts.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}));
+          end
+          if isfield(sd.exper,'trialinfo_allEv')
+            exper.trialinfo_allEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}) = cat(1,exper.trialinfo_allEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}),sd.exper.trialinfo_allEv.(exper.sesStr{ses}).(exper.eventValues{ses}{evVal}));
           end
         end
         
