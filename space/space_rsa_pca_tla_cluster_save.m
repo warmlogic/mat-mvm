84f2795d8b4d364deb8080584d90d0d6eb2d5441
Matt Mollison <matt.mollison@gmail.com>
2014-07-10 15:43:55 -0600

    minor

--- space/space_rsa_pca_tla_cluster_save.m
+++ space/space_rsa_pca_tla_cluster_save.m
@@ -71,8 +71,8 @@ end
 
 dataTypes = {'img_RgH_rc_spac', 'img_RgH_rc_mass','img_RgH_fo_spac', 'img_RgH_fo_mass'};
 
-% allROIs = {{'LPI2','LPS','LT','RPI2','RPS','RT'},{'center109'},{'LPS','RPS'},{'LT','RT'}};
-allROIs = {{'LPI2','RPI2'}};
+allROIs = {{'LPI2','LPS','LT','RPI2','RPS','RT'},{'center109'},{'LPS','RPS'},{'LT','RT'},{'LPI2','RPI2'}};
+% allROIs = {{'LPI2','RPI2'}};
 
 allLats = {[0.0 0.2; 0.2 0.4; 0.4 0.6; 0.6 0.8; 0.8 1.0; ...
   0.1 0.3; 0.3 0.5; 0.5 0.7; 0.7 0.9; ...
@@ -91,7 +91,8 @@ elseif strcmp(origDataType,'hilbert')
   allFreqs = {[4 8; 8 12; 12 30; 30 50] [4 8] [8 12] [12 30] [30 50]};
 end
 
-allSimMethod = {'cosine'};
+% allSimMethod = {'cosine'};
+allSimMethod = {'correlation'};
 
 % sim_method = 'cosine';
 % sim_method = 'correlation';
