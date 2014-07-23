0b1308417bc69126528a58ef4aa30c0a5117432d
Matt Mollison <matt.mollison@gmail.com>
2014-07-23 11:52:24 -0600

    setup for MFF

--- space/space_ft_seg_tla.m
+++ space/space_ft_seg_tla.m
@@ -17,7 +17,8 @@ exper.sampleRate = 250;
 % type of NS file for FieldTrip to read; raw or sbin must be put in
 % dirs.dataroot/ns_raw; egis must be put in dirs.dataroot/ns_egis
 % exper.eegFileExt = 'egis';
-exper.eegFileExt = 'raw';
+% exper.eegFileExt = 'raw';
+exper.eegFileExt = 'mff';
 
 % types of events to find in the NS file; these must be the same as the
 % events in the NS files; or space_trialfun.m must be set up to find the
@@ -35,8 +36,8 @@ exper.prepost = {[-1.0 2.0; -1.0 2.0; -1.0 2.0; -1.0 2.0]};
 % exper.prepost = {[-0.2 1.0], [-0.2 1.0]};
 
 exper.subjects = {
-  'SPACE001'; % low trial counts
-  'SPACE002';
+  %'SPACE001'; % low trial counts
+  'SPACE002'; % already processed
   'SPACE003';
   'SPACE004';
   'SPACE005';
@@ -51,9 +52,9 @@ exper.subjects = {
   'SPACE014';
   'SPACE015';
   'SPACE016';
-  'SPACE017'; % really noisy EEG, half of ICA components rejected
+  %'SPACE017'; % really noisy EEG, half of ICA components rejected
   'SPACE018';
-  'SPACE019'; % low trial counts
+  %'SPACE019'; % low trial counts
   'SPACE020';
   'SPACE021';
   'SPACE022';
@@ -66,8 +67,8 @@ exper.subjects = {
   'SPACE025';
   'SPACE026';
   'SPACE028';
-  'SPACE030'; % low trial counts
-  'SPACE032';
+  %'SPACE030'; % low trial counts
+  'SPACE032'; % already processed
   'SPACE034';
   'SPACE047';
   'SPACE049';
@@ -126,7 +127,8 @@ ana.elec = ft_read_sens(files.elecfile,'fileformat',files.locsFormat);
 ana.segFxn = 'seg2ft';
 
 ana.continuous = 'yes';
-ana.trialFxn = 'space_trialfun';
+% ana.trialFxn = 'space_trialfun';
+ana.trialFxn = 'space_trialfun_mff';
 ana.allowTrialOverlap = true;
 ana.renumberSamplesContiguous = true;
 % files used when adding metadata to segmented trials
@@ -177,7 +179,7 @@ ana.artifact.type = {'nsClassic','ftAuto'};
 % ana.artifact.type = {'nsClassic'};
 
 % set up for nsClassic
-ana.artifact.checkArtSec = [-0.2 1.0];
+ana.artifact.checkArtSec = [0 1.0];
 ana.artifact.blink_threshold = 70;
 ana.artifact.fast_threshold = 100;
 ana.artifact.diff_threshold = 50;
