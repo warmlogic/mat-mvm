f6a316d7e7951557510a1c536c769095d4fdaad8
Matt Mollison <matt.mollison@gmail.com>
2014-08-14 16:08:37 -0600

    one more ROI

--- space/space_rsa_pca_pow_cluster_save.m
+++ space/space_rsa_pca_pow_cluster_save.m
@@ -85,7 +85,7 @@ end
 
 % allROIs = {{'LPI2','LPS','LT','RPI2','RPS','RT'},{'center109'},{'LPS','RPS'},{'LT','RT'},{'LPI2','RPI2'},{'LAS2','FS','RAS2'},{'LFP','FC','RFP'}};
 % allROIs = {{'FS'},{'C'},{'PS'},{'PI'},{'LT'},{'RT'},{'LPS'},{'RPS'}};
-allROIs = {{'LPS','RPS'},{'LT','RT'},{'PS'}};
+allROIs = {{'LPS','RPS'},{'LT','RT'},{'PS'},{'LPI2','LPS','LT','RPI2','RPS','RT'}};
 
 allLats = {[0.0 0.2; 0.2 0.4; 0.4 0.6; 0.6 0.8; 0.8 1.0; ...
   0.1 0.3; 0.3 0.5; 0.5 0.7; 0.7 0.9; ...
