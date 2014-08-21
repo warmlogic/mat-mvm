3fb82a2a91890b6523fd57c0dd114b143be7e175
Matt Mollison <matt.mollison@gmail.com>
2014-08-20 23:39:17 -0600

    support for multiple time bins

--- ft_general/mm_ft_lineplotTFR_multiSes.m
+++ ft_general/mm_ft_lineplotTFR_multiSes.m
@@ -1,13 +1,10 @@
 function mm_ft_lineplotTFR_multiSes(cfg_ft,cfg_plot,ana,exper,files,dirs,sesNum,data)
 %MM_FT_LINEPLOTTFR_MULTISES Line plot of Time-Freq data
-%   
-
-% warning('Do not use this function for now.');
+%
 
 if ~isfield(cfg_ft,'parameter')
   cfg_ft.parameter = 'powspctrm';
 end
-
 
 % check on the labels
 if ~isfield(cfg_plot,'xlabel')
@@ -36,12 +33,6 @@ if ~isfield(cfg_plot,'rename_conditions')
   cfg_plot.rename_conditions = cfg_plot.plot_order;
 end
 
-
-% if   s_rc_mark = 'ro';
-%   s_fo_mark = 'mo';
-%   m_rc_mark = 'bx';
-%   m_fo_mark = 'cx';
-
 if ~isfield(cfg_plot,'linespec')
   % cfg_plot.linespec = 'k--o';
   cfg_plot.linespec = {'bo','cx','ro','mx'};
@@ -67,7 +58,6 @@ end
 if ~isfield(cfg_plot,'axisxy')
   cfg_plot.axisxy = false;
 end
-
 
 % set up how the lines will look
 if ~isfield(cfg_plot,'linewidth')
@@ -97,62 +87,13 @@ elseif isfield(cfg_plot,'roi')
 end
 
 numSub = length(exper.subjects) - sum(exper.badSub(:,sesNum));
-cfg_ana = struct;
-
-% get times, data, SEM
-for evVal = 1:length(cfg_plot.conditions)
-  ev = cfg_plot.conditions{evVal};
-  %cfg_ana.values.(ev) = nan(numSub,length(exper.sessions));
-  cfg_ana.values.(ev) = nan(numSub,1);
-  goodSubInd = 0;
-  for sub = 1:length(exper.subjects)
-    %for ses = 1:length(exper.sessions)
-    if exper.badSub(sub,sesNum)
-      fprintf('Skipping bad subject: %s\n',exper.subjects{sub});
-      continue
-    else
-      goodSubInd = goodSubInd + 1;
-      
-      % get the right channels (on an individual subject basis)
-      if ismember(cfg_plot.roi,ana.elecGroupsStr)
-        cfg_ana.channel = cat(2,ana.elecGroups{ismember(ana.elecGroupsStr,cfg_plot.roi)});
-        cfg_ana.chansel = ismember(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.label,cfg_ana.channel);
-        cfg_plot.chan_str = sprintf(repmat('%s_',1,length(cfg_plot.roi)),cfg_plot.roi{:});
-      else
-        % find the channel indices for averaging
-        cfg_ana.chansel = ismember(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.label,cfg_plot.roi);
-        cfg_plot.chan_str = sprintf(repmat('%s_',1,length(cfg_ft.channel)),cfg_ft.channel{:});
-      end
-      
-      cfg_ana.timesel.(ev) = false(size(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.time));
-      tbeg = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.time,cfg_plot.latency(1));
-      tend = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.time,cfg_plot.latency(2));
-      cfg_ana.timesel.(ev)(tbeg:tend) = true;
-      
-      cfg_ana.freqsel.(ev) = false(size(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.freq));
-      fbeg = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.freq,cfg_plot.freqs(1));
-      fend = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.freq,cfg_plot.freqs(2));
-      cfg_ana.freqsel.(ev)(fbeg:fend) = true;
-      
-      cfg_ana.values.(ev)(goodSubInd) = mean(mean(mean(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.(cfg_ft.parameter)(cfg_ana.chansel,cfg_ana.freqsel.(ev),cfg_ana.timesel.(ev)),3),2),1);
-    end
-    %end % ses
-  end % sub
-  cfg_ana.sem.(ev) = std(cfg_ana.values.(ev))/sqrt(length(cfg_ana.values.(ev)));
-end % evVal
-
-
-% do the mean amplitude line plots
-if ~isfield(cfg_plot,'ylim')
-  cfg_plot.ylim = eval(sprintf('[floor(min([%s])) ceil(max([%s]))]',sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:}),sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:})));
-elseif isfield(cfg_plot,'ylim') && strcmp(cfg_plot.ylim,'minmax')
-  cfg_plot.ylim = eval(sprintf('[floor(min([%s])) ceil(max([%s]))]',sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:}),sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:})));
-end
 
 if ~isfield(cfg_plot,'xlim')
-  cfg_plot.xlim = [0 length(cfg_plot.conditions)+0.5];
+  cfg_plot.xlim = [0 (length(cfg_plot.conditions)*size(cfg_plot.latency,1))+0.5];
+  if size(cfg_plot.latency,1) > 1
+    cfg_plot.xlim(2) = cfg_plot.xlim(2) + size(cfg_plot.latency,1) - 1;
+  end
 end
-
 
 figure
 % plot the lines
@@ -160,43 +101,118 @@ figure
 hold on
 
 plot([cfg_plot.xlim(1) cfg_plot.xlim(2)],[0 0],'k--'); % horizontal
-plot([0 0],[cfg_plot.ylim(1) cfg_plot.ylim(2)],'k--'); % vertical
-
-h_d = nan(1,length(cfg_plot.plot_order));
-for c = 1:length(cfg_plot.plot_order)
-  % errorbars
-  h = errorbar(c,mean(cfg_ana.values.(cfg_plot.plot_order{c}),1),cfg_ana.sem.(cfg_plot.plot_order{c}),cfg_plot.linespec{c},'LineWidth',cfg_plot.errwidth);
-  % remove errorbar ends
-  if cfg_plot.removeErrBarEnds
-    chil = get(h,'Children');
-    xdata = get(chil(2),'XData');
-    ydata = get(chil(2),'YData');
-    xdata(cfg_plot.errBarEndMarkerInd) = NaN;
-    ydata(cfg_plot.errBarEndMarkerInd) = NaN;
-    set(chil(2),'XData',xdata);
-    set(chil(2),'YData',ydata);
-    set(h,'Children',chil);
-  end
-  % plot the markers
-  h_d(c) = plot(c,mean(cfg_ana.values.(cfg_plot.plot_order{c}),1),cfg_plot.linespec{c},'LineWidth',cfg_plot.linewidth,'MarkerSize',cfg_plot.marksize,'MarkerFaceColor',cfg_plot.markcolor{c});
+if cfg_plot.xlim(1) ~= 0
+  plot([0 0],[cfg_plot.ylim(1) cfg_plot.ylim(2)],'k--'); % vertical
 end
+
+lat_str = '';
+xIndCounter = 0;
+for lat = 1:size(cfg_plot.latency,1)
+  lat_str = sprintf('%s_%d_%d',lat_str,round(cfg_plot.latency(lat,1)*1000),round(cfg_plot.latency(lat,2)*1000));
+  
+  if lat > 1
+    plot([xIndCounter+1 xIndCounter+1],[cfg_plot.ylim(1) cfg_plot.ylim(2)],'k-'); % vertical
+    xIndCounter = xIndCounter + 1;
+  end
+  
+  cfg_ana = struct;
+  
+  % get times, data, SEM
+  for evVal = 1:length(cfg_plot.conditions)
+    ev = cfg_plot.conditions{evVal};
+    %cfg_ana.values.(ev) = nan(numSub,length(exper.sessions));
+    cfg_ana.values.(ev) = nan(numSub,1);
+    goodSubInd = 0;
+    for sub = 1:length(exper.subjects)
+      %for ses = 1:length(exper.sessions)
+      if exper.badSub(sub,sesNum)
+        fprintf('Skipping bad subject: %s\n',exper.subjects{sub});
+        continue
+      else
+        goodSubInd = goodSubInd + 1;
+        
+        % get the right channels (on an individual subject basis)
+        if ismember(cfg_plot.roi,ana.elecGroupsStr)
+          cfg_ana.channel = cat(2,ana.elecGroups{ismember(ana.elecGroupsStr,cfg_plot.roi)});
+          cfg_ana.chansel = ismember(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.label,cfg_ana.channel);
+          if length(cfg_plot.roi) <= 10
+            cfg_plot.chan_str = sprintf(repmat('%s_',1,length(cfg_plot.roi)),cfg_plot.roi{:});
+          else
+            cfg_plot.chan_str = sprintf('%dROI',length(cfg_plot.roi));
+          end
+        else
+          % find the channel indices for averaging
+          cfg_ana.chansel = ismember(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.label,cfg_plot.roi);
+          if length(cfg_plot.roi) <= 10
+          cfg_plot.chan_str = sprintf(repmat('%s_',1,length(cfg_ft.channel)),cfg_ft.channel{:});
+          else
+            cfg_plot.chan_str = sprintf('%dROI',length(cfg_plot.roi));
+          end
+        end
+        
+        cfg_ana.timesel.(ev) = false(size(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.time));
+        tbeg = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.time,cfg_plot.latency(lat,1));
+        tend = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.time,cfg_plot.latency(lat,2));
+        cfg_ana.timesel.(ev)(tbeg:tend) = true;
+        
+        cfg_ana.freqsel.(ev) = false(size(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.freq));
+        fbeg = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.freq,cfg_plot.freqs(1));
+        fend = nearest(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.freq,cfg_plot.freqs(2));
+        cfg_ana.freqsel.(ev)(fbeg:fend) = true;
+        
+        cfg_ana.values.(ev)(goodSubInd) = mean(mean(mean(data.(exper.sesStr{sesNum}).(ev).sub(sub).data.(cfg_ft.parameter)(cfg_ana.chansel,cfg_ana.freqsel.(ev),cfg_ana.timesel.(ev)),3),2),1);
+      end
+      %end % ses
+    end % sub
+    cfg_ana.sem.(ev) = std(cfg_ana.values.(ev))/sqrt(length(cfg_ana.values.(ev)));
+  end % evVal
+  
+  % do the mean amplitude line plots
+  if ~isfield(cfg_plot,'ylim')
+    cfg_plot.ylim = eval(sprintf('[floor(min([%s])) ceil(max([%s]))]',sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:}),sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:})));
+  elseif isfield(cfg_plot,'ylim') && strcmp(cfg_plot.ylim,'minmax')
+    cfg_plot.ylim = eval(sprintf('[floor(min([%s])) ceil(max([%s]))]',sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:}),sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:})));
+  end
+  
+  h_d = nan(1,length(cfg_plot.plot_order));
+  for c = 1:length(cfg_plot.plot_order)
+    xIndCounter = xIndCounter + 1;
+    
+    % errorbars
+    h = errorbar(xIndCounter,mean(cfg_ana.values.(cfg_plot.plot_order{c}),1),cfg_ana.sem.(cfg_plot.plot_order{c}),cfg_plot.linespec{c},'LineWidth',cfg_plot.errwidth);
+    % remove errorbar ends
+    if cfg_plot.removeErrBarEnds
+      chil = get(h,'Children');
+      xdata = get(chil(2),'XData');
+      ydata = get(chil(2),'YData');
+      xdata(cfg_plot.errBarEndMarkerInd) = NaN;
+      ydata(cfg_plot.errBarEndMarkerInd) = NaN;
+      set(chil(2),'XData',xdata);
+      set(chil(2),'YData',ydata);
+      set(h,'Children',chil);
+    end
+    % plot the markers
+    h_d(c) = plot(xIndCounter,mean(cfg_ana.values.(cfg_plot.plot_order{c}),1),cfg_plot.linespec{c},'LineWidth',cfg_plot.linewidth,'MarkerSize',cfg_plot.marksize,'MarkerFaceColor',cfg_plot.markcolor{c});
+  end
+end
+
 if cfg_plot.plotLegend
   legend(h_d,cfg_plot.legendtext,'Location',cfg_plot.legendloc);
 end
 
 hold off
 
-set(gcf,'Name',sprintf('%s, %.1fs--%.1f s',strrep(cfg_plot.chan_str,'_',' '),cfg_plot.latency(1),cfg_plot.latency(2)))
+set(gcf,'Name',sprintf('%s, %s s',strrep(cfg_plot.chan_str,'_',' '),lat_str(2:end)))
 
 % make it look good
-axis([.5 (length(cfg_plot.rename_conditions) + .5) cfg_plot.ylim(1) cfg_plot.ylim(2)])
+axis([.5 (xIndCounter + .5) cfg_plot.ylim(1) cfg_plot.ylim(2)])
 xlabel(cfg_plot.xlabel);
 ylabel(cfg_plot.ylabel);
-set(gca,'XTick',(1:length(cfg_plot.rename_conditions)))
+set(gca,'XTick',(1:xIndCounter))
 if ~isempty(cfg_plot.xlabel)
   set(gca,'XTickLabel',strrep(cfg_plot.rename_conditions,'_','-'))
 else
-  set(gca,'XTickLabel',repmat({''},1,length(cfg_plot.rename_conditions)))
+  set(gca,'XTickLabel',repmat({''},1,xIndCounter))
 end
 set(gca,'YTick',(cfg_plot.ylim(1):.5:cfg_plot.ylim(2)))
 axis square
@@ -211,8 +227,8 @@ if ~isfield(files,'figFontName')
 end
 publishfig(gcf,0,[],[],files.figFontName);
 if files.saveFigs
-  cfg_plot.figfilename = sprintf('tfr_line_ga_%s%s%d_%d_%d_%d%s',sprintf(repmat('%s_',1,length(cfg_plot.plot_order)),cfg_plot.plot_order{:}),cfg_plot.chan_str,round(cfg_plot.latency(1)*1000),round(cfg_plot.latency(2)*1000),round(cfg_plot.freqs(1)),round(cfg_plot.freqs(2)),cfg_plot.label_str);
-  dirs.saveDirFigsLine = fullfile(dirs.saveDirFigs,'tfr_line');
+  cfg_plot.figfilename = sprintf('tfr_avg_ga_%s%s%s_%d_%d%s',sprintf(repmat('%s_',1,length(cfg_plot.plot_order)),cfg_plot.plot_order{:}),cfg_plot.chan_str,lat_str,round(cfg_plot.freqs(1)),round(cfg_plot.freqs(2)),cfg_plot.label_str);
+  dirs.saveDirFigsLine = fullfile(dirs.saveDirFigs,'tfr_avg');
   if ~exist(dirs.saveDirFigsLine,'dir')
     mkdir(dirs.saveDirFigsLine)
   end
@@ -225,47 +241,6 @@ if files.saveFigs
   end
   print(gcf,sprintf('-d%s',files.figPrintFormat),sprintf('-r%d',files.figPrintRes),fullfile(dirs.saveDirFigsLine,cfg_plot.figfilename));
 end
-
-
-
-
-
-
-
-
-
-
-% % make sure it's set up for the for loop
-% if ~isfield(cfg_plot,'condMethod')
-%   if ~iscell(cfg_plot.conditions) && (strcmp(cfg_plot.conditions,'all') || strcmp(cfg_plot.conditions,'all_across_types') || strcmp(cfg_plot.conditions,'all_within_types'))
-%     cfg_plot.condMethod = 'single';
-%   elseif iscell(cfg_plot.conditions) && ~iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-%     cfg_plot.condMethod = 'single';
-%   elseif iscell(cfg_plot.conditions) && iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions{1}) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-%     cfg_plot.condMethod = 'single';
-%   else
-%     cfg_plot.condMethod = [];
-%   end
-% end
-% cfg_plot.conditions = mm_ft_checkConditions(cfg_plot.conditions,ana,cfg_plot.condMethod);
-
-% for typ = 1:length(cfg_plot.conditions)
-%   cfg = [];
-%   cfg.conditions = cfg_plot.conditions{typ};
-%   cfg.data_str = 'data';
-%   cfg.is_ga = cfg_plot.is_ga;
-%   cfg.excludeBadSub = cfg_plot.excludeBadSub;
-%   ana_str = mm_ft_catSubStr(cfg,exper);
-%   
-%   figure
-%   eval(sprintf('ft_singleplotER(cfg_ft,%s);',ana_str));
-%   legend(strrep(cfg_plot.conditions{typ},'_','-'),'Location',cfg_plot.legendloc);
-%   set(gcf,'Name',sprintf(repmat('%s ',1,length(cfg_plot.roi)),cfg_plot.roi{:}));
-%   
-%   plot([cfg_ft.xlim(1) cfg_ft.xlim(2)],[0 0],'k--'); % horizontal
-%   plot([0 0],[cfg_ft.ylim(1) cfg_ft.ylim(2)],'k--'); % vertical
-% end
-
 
 
 end
