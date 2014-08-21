3934e93dfd5f5110a0b2087dd745a64d3a3d5d6c
Matt Mollison <matt.mollison@gmail.com>
2014-08-20 23:39:23 -0600

    cleanup

--- ft_general/mm_ft_lineplotER_multiSes.m
+++ ft_general/mm_ft_lineplotER_multiSes.m
@@ -2,15 +2,9 @@ function mm_ft_lineplotER_multiSes(cfg_ft,cfg_plot,ana,exper,files,dirs,sesNum,d
 %MM_FT_LINEPLOTER_MULTISES Line plot of ERP data
 %   
 
-% warning('Do not use this function for now.');
-
-% if ~isfield(cfg_ft,'parameter')
-%   error('Must define cfg_ft.parameter for ft_singleplotER');
-% end
 if ~isfield(cfg_ft,'parameter')
   cfg_ft.parameter = 'avg';
 end
-
 
 % check on the labels
 if ~isfield(cfg_plot,'xlabel')
@@ -39,12 +33,6 @@ if ~isfield(cfg_plot,'rename_conditions')
   cfg_plot.rename_conditions = cfg_plot.plot_order;
 end
 
-
-% if   s_rc_mark = 'ro';
-%   s_fo_mark = 'mo';
-%   m_rc_mark = 'bx';
-%   m_fo_mark = 'cx';
-
 if ~isfield(cfg_plot,'linespec')
   % cfg_plot.linespec = 'k--o';
   cfg_plot.linespec = {'bo','cx','ro','mx'};
@@ -71,7 +59,6 @@ if ~isfield(cfg_plot,'axisxy')
   cfg_plot.axisxy = false;
 end
 
-
 % set up how the lines will look
 if ~isfield(cfg_plot,'linewidth')
   cfg_plot.linewidth = 3;
@@ -87,24 +74,6 @@ if ~isfield(cfg_plot,'removeErrBarEnds')
   cfg_plot.removeErrBarEnds = true;
 end
 
-
-% if ~isfield(cfg_ft,'fontsize')
-%   cfg_ft.fontsize = 9;
-% end
-% if ~isfield(cfg_ft,'graphcolor')
-%   cfg_ft.graphcolor = 'rbkgcmyrbkgcmyrbkgcmy';
-% end
-% if ~isfield(cfg_ft,'linestyle')
-%   cfg_ft.linestyle = {'-','--','-.','-','--','-.','-','--','-.','-','--','-.','-','--','-.','-','--','-.','-','--','-.'};
-% end
-% if ~isfield(cfg_ft,'markerstyle')
-%   cfg_ft.markerstyle = {'o','o','o','o','o','o','o','s','s','s','s','s','s','s'};
-% end
-% if ~isfield(cfg_plot,'excludeBadSub')
-%   cfg_plot.excludeBadSub = 1;
-% end
-
-
 if ~isfield(cfg_plot,'roi')
   error('Must specify either ROI names or channel names in cfg_plot.roi');
 elseif isfield(cfg_plot,'roi')
@@ -116,49 +85,6 @@ elseif isfield(cfg_plot,'roi')
     cfg_ft.channel = cfg_plot.roi;
   end
 end
-
-% cfg_ga = [];
-% cfg_ga.parameter = 'avg';
-% cfg_ga.latency = cfg_ft.latency;
-% cfg_ga.keepindividual = 'no';
-% cfg_ga.channel = cfg_ft.channel;
-% 
-% for ev = 1:length(cfg_plot.conditions)
-%   cfg = [];
-%   cfg.conditions = cfg_plot.conditions{ev};
-%   cfg.data_str = 'data';
-%   cfg.is_ga = cfg_plot.is_ga;
-%   cfg.excludeBadSub = cfg_plot.excludeBadSub;
-%   ana_str = mm_catSubStr_multiSes2(cfg,exper,sesNum);
-%   
-%   ga = eval(sprintf('ft_timelockgrandaverage(cfg_ga,%s);',ana_str.(cfg_plot.conditions{ev})));
-%   
-%   %[data_sel1,data_sel2] = eval(sprintf('ft_selectdata_new(cfg_ft,%s);',ana_str.(cfg_plot.conditions{ev})));
-%   
-%   figure
-%   eval(sprintf('ft_singleplotER(cfg_ft,%s);',ana_str));
-%   legend_conds = repmat(cfg_plot.conditions,1,length(sesNum));
-%   if length(sesNum) > 1
-%     condInd = 0;
-%     for ses = 1:length(sesNum)
-%       %legend(strrep(cfg_plot.conditions,'_','-'),'Location',cfg_plot.legendloc);
-%       for c = 1:length(cfg_plot.conditions)
-%         condInd = condInd + 1;
-%         legend_conds{condInd} = sprintf('%s-%s',legend_conds{condInd},exper.sesStr{sesNum(ses)});
-%       end
-%     end
-%   end
-%   legend(strrep(legend_conds,'_','-'),'Location',cfg_plot.legendloc);
-%   set(gcf,'Name',sprintf(repmat('%s ',1,length(cfg_plot.roi)),cfg_plot.roi{:}));
-%   
-%   plot([cfg_ft.xlim(1) cfg_ft.xlim(2)],[0 0],'k--'); % horizontal
-%   plot([0 0],[cfg_ft.ylim(1) cfg_ft.ylim(2)],'k--'); % vertical
-%   
-%   if cfg_plot.axisxy
-%     %axis xy;
-%     set(gca,'YDir','reverse');
-%   end
-% end
 
 numSub = length(exper.subjects) - sum(exper.badSub(:,sesNum));
 cfg_ana = struct;
@@ -203,8 +129,6 @@ for evVal = 1:length(cfg_plot.conditions)
   cfg_ana.sem.(ev) = std(cfg_ana.values.(ev))/sqrt(length(cfg_ana.values.(ev)));
 end % evVal
 
-
-
 % do the mean amplitude line plots
 if ~isfield(cfg_plot,'ylim')
   cfg_plot.ylim = eval(sprintf('[floor(min([%s])) ceil(max([%s]))]',sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:}),sprintf(repmat('mean(cfg_ana.values.%s,1) ',1,length(cfg_plot.conditions)),cfg_plot.conditions{:})));
@@ -215,7 +139,6 @@ end
 if ~isfield(cfg_plot,'xlim')
   cfg_plot.xlim = [0 length(cfg_plot.conditions)+0.5];
 end
-
 
 
 figure
@@ -275,8 +198,8 @@ if ~isfield(files,'figFontName')
 end
 publishfig(gcf,0,[],[],files.figFontName);
 if files.saveFigs
-  cfg_plot.figfilename = sprintf('tla_line_ga_%s%s%d_%d%s',sprintf(repmat('%s_',1,length(cfg_plot.plot_order)),cfg_plot.plot_order{:}),cfg_plot.chan_str,round(cfg_plot.latency(1)*1000),round(cfg_plot.latency(2)*1000),cfg_plot.label_str);
-  dirs.saveDirFigsLine = fullfile(dirs.saveDirFigs,'tla_line');
+  cfg_plot.figfilename = sprintf('tla_avg_ga_%s%s%d_%d%s',sprintf(repmat('%s_',1,length(cfg_plot.plot_order)),cfg_plot.plot_order{:}),cfg_plot.chan_str,round(cfg_plot.latency(1)*1000),round(cfg_plot.latency(2)*1000),cfg_plot.label_str);
+  dirs.saveDirFigsLine = fullfile(dirs.saveDirFigs,'tla_avg');
   if ~exist(dirs.saveDirFigsLine,'dir')
     mkdir(dirs.saveDirFigsLine)
   end
@@ -290,47 +213,4 @@ if files.saveFigs
   print(gcf,sprintf('-d%s',files.figPrintFormat),sprintf('-r%d',files.figPrintRes),fullfile(dirs.saveDirFigsLine,cfg_plot.figfilename));
 end
 
-
-
-
-
-
-
-
-
-
-% % make sure it's set up for the for loop
-% if ~isfield(cfg_plot,'condMethod')
-%   if ~iscell(cfg_plot.conditions) && (strcmp(cfg_plot.conditions,'all') || strcmp(cfg_plot.conditions,'all_across_types') || strcmp(cfg_plot.conditions,'all_within_types'))
-%     cfg_plot.condMethod = 'single';
-%   elseif iscell(cfg_plot.conditions) && ~iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-%     cfg_plot.condMethod = 'single';
-%   elseif iscell(cfg_plot.conditions) && iscell(cfg_plot.conditions{1}) && length(cfg_plot.conditions{1}) == 1 && (strcmp(cfg_plot.conditions{1},'all') || strcmp(cfg_plot.conditions{1},'all_across_types') || strcmp(cfg_plot.conditions{1},'all_within_types'))
-%     cfg_plot.condMethod = 'single';
-%   else
-%     cfg_plot.condMethod = [];
-%   end
-% end
-% cfg_plot.conditions = mm_ft_checkConditions(cfg_plot.conditions,ana,cfg_plot.condMethod);
-
-% for typ = 1:length(cfg_plot.conditions)
-%   cfg = [];
-%   cfg.conditions = cfg_plot.conditions{typ};
-%   cfg.data_str = 'data';
-%   cfg.is_ga = cfg_plot.is_ga;
-%   cfg.excludeBadSub = cfg_plot.excludeBadSub;
-%   ana_str = mm_ft_catSubStr(cfg,exper);
-%   
-%   figure
-%   eval(sprintf('ft_singleplotER(cfg_ft,%s);',ana_str));
-%   legend(strrep(cfg_plot.conditions{typ},'_','-'),'Location',cfg_plot.legendloc);
-%   set(gcf,'Name',sprintf(repmat('%s ',1,length(cfg_plot.roi)),cfg_plot.roi{:}));
-%   
-%   plot([cfg_ft.xlim(1) cfg_ft.xlim(2)],[0 0],'k--'); % horizontal
-%   plot([0 0],[cfg_ft.ylim(1) cfg_ft.ylim(2)],'k--'); % vertical
-% end
-
-
-
 end
-
