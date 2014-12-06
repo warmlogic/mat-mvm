520a96f31726a8aa982c90a218b1fc6545c32143
Matt Mollison <matt.mollison@gmail.com>
2014-12-05 18:23:58 -0700

    comment out those that have been processed

--- space2/space2_ftprocess_tla_wrapper.m
+++ space2/space2_ftprocess_tla_wrapper.m
@@ -60,28 +60,28 @@ exper.prepost = {[-1.0 2.0; -1.0 2.0; -1.0 2.0]};
 % exper.prepost = {[-1.0 2.0]};
 
 exper.subjects = {
-  'SPACE2001';
-  'SPACE2002';
-  %'SPACE2003'; % DNF session 2
-  'SPACE2004';
-  'SPACE2005';
-  'SPACE2006';
-  'SPACE2007'; % terrible performance
-  'SPACE2008';
-  %'SPACE2009'; % DNF session 2
-  'SPACE2010';
-  'SPACE2011';
-  'SPACE2012';
-  %'SPACE2013'; % didn't record EEG, stopped session 1 in middle
-  'SPACE2014';
-  'SPACE2015';
-  'SPACE2016';
-  'SPACE2017'; % terrible performance
-  'SPACE2018';
-  'SPACE2019';
-  'SPACE2020';
-  'SPACE2021';
-  'SPACE2022';
+%   'SPACE2001';
+%   'SPACE2002';
+%   %'SPACE2003'; % DNF session 2
+%   'SPACE2004';
+%   'SPACE2005';
+%   'SPACE2006';
+%   'SPACE2007'; % terrible performance
+%   'SPACE2008';
+%   %'SPACE2009'; % DNF session 2
+%   'SPACE2010';
+%   'SPACE2011';
+%   'SPACE2012';
+%   %'SPACE2013'; % didn't record EEG, stopped session 1 in middle
+%   'SPACE2014';
+%   'SPACE2015';
+%   'SPACE2016';
+%   'SPACE2017'; % terrible performance
+%   'SPACE2018';
+%   'SPACE2019';
+%   'SPACE2020';
+%   'SPACE2021';
+%   'SPACE2022';
   %'SPACE2023'; % no ses2
   %'SPACE2024'; % no ses2
   'SPACE2025';
